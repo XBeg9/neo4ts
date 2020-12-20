48fb0c9c897613fe1472c127b260b39e30e5b3c8
Fedya Skitsko <fedya@skitsko.com>
2020-12-20 10:57:21 +0200

    feat: Export modules

--- packages/builder/src/index.ts
+++ packages/builder/src/index.ts
@@ -1,1 +1,9 @@
 export * from './builder';
+export * from './match';
+export * from './node-relation';
+export * from './node-with-param';
+export * from './node';
+export * from './order-by';
+export * from './return';
+export * from './where';
+
